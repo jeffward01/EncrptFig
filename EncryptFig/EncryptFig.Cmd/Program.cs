9549946ef769f7527fcee96323a9e8bced980717
Ward <Jeff.Ward@umusic.com>
2017-01-19 11:58:45 -0800

    Version 1 of EncryptFigComplete

---
+++ EncryptFig/EncryptFig.Cmd/Program.cs
@@ -0,0 +1,75 @@
+﻿using EncryptFig.Cmd.Services;
+using System;
+
+namespace EncryptFig.Cmd
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            WelcomeMessage();
+        }
+
+        private static void WelcomeMessage()
+        {
+            var service = new ConfigService();
+            Console.WriteLine("Welcome to Enrypt, your Encryption and Decryption tool... \n \n ");
+
+            Console.WriteLine("Configuration:");
+            Console.WriteLine(service.OutputConfig());
+            Console.WriteLine("\n \n");
+            Console.WriteLine("Please type either encrypt or decrypt to perform the action \n ('e' or 'd' is also acceptable) \n \n Input:");
+            var input = Console.ReadLine();
+            if (!ReadInput(input))
+            {
+                Console.Clear();
+                WelcomeMessage();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private static bool ReadInput(string input)
+        {
+            input = input.ToLower();
+            input = input.Trim();
+            var myInput = input[0];
+
+            if (myInput == 'e' || myInput == 'd')
+            {
+                _selectAction(myInput);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void _selectAction(char input)
+        {
+            var service = new ConfigService();
+            if (input == 'e')
+            {
+                Console.WriteLine(service.EnCrypt());
+                _successMessage();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(service.DeCrypt());
+                _successMessage();
+                Console.ReadLine();
+            }
+        }
+
+        private static void _successMessage()
+        {
+            Console.WriteLine("\n \n Success!!! \n \n press any key to exit....");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ EncryptFig/EncryptFig.Cmd/Services/ConfigService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Text;
+
+namespace EncryptFig.Cmd.Services
+{
+    public class ConfigService
+    {
+        private static readonly string[] _nodeArray = ConfigurationManager.AppSettings["Nodes"].Split(';');
+        private static readonly string _webConfigFolder = ConfigurationManager.AppSettings["WebConfigFolder"];
+
+        public string OutputConfig()
+        {
+            return buildMessage(_webConfigFolder, _nodeArray);
+        }
+
+        private string buildMessage(string folderPath, string[] nodes)
+        {
+            var myString = new StringBuilder();
+            myString.AppendLine("WebConfigFolder: " + folderPath);
+            myString.AppendLine("Nodes to be encrypted/decrypted:");
+            var count = 1;
+            foreach (var node in nodes)
+            {
+                myString.AppendLine(count + "). " + node);
+                count++;
+            }
+            return myString.ToString();
+        }
+
+        public string EnCrypt()
+        {
+            var myProcessInfo = new System.Diagnostics.ProcessStartInfo(); //Initializes a new ProcessStartInfo of name myProcessInfo
+            myProcessInfo.FileName = Environment.ExpandEnvironmentVariables("%SystemRoot%") + @"\System32\cmd.exe"; //Sets the FileName property of myProcessInfo to %SystemRoot%\System32\cmd.exe where %SystemRoot% is a system variable which is expanded using Environment.ExpandEnvironmentVariables
+            //myProcessInfo.Arguments = "&"; //Sets the arguments to cd..
+            myProcessInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; //Sets the WindowStyle of myProcessInfo which indicates the window state to use when the process is started to Hidden
+            myProcessInfo.Verb = "runas"; //The process should start with elevated permissions
+
+            myProcessInfo.RedirectStandardInput = true;
+            myProcessInfo.RedirectStandardOutput = true;
+            myProcessInfo.UseShellExecute = false;
+
+            var process = Process.Start(myProcessInfo);
+
+            if (process != null)
+            {
+                process.StandardInput.WriteLine("cd C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319");
+                
+                foreach (var node in _nodeArray)
+                {
+                    string commandString= "@ASPNET_REGIIS -pef " + ""+ node +"" +" "+  ""+_webConfigFolder +"";
+                    process.StandardInput.WriteLine(commandString);
+                }
+         
+                //process.StandardInput.WriteLine("yourCommand.exe arg1 arg2");
+
+                process.StandardInput.Close(); // line added to stop process from hanging on ReadToEnd()
+
+                var outputString = process.StandardOutput.ReadToEnd();
+                return outputString;
+            }
+            return string.Empty;
+        }
+
+        public string DeCrypt()
+        {
+            var myProcessInfo = new System.Diagnostics.ProcessStartInfo(); //Initializes a new ProcessStartInfo of name myProcessInfo
+            myProcessInfo.FileName = Environment.ExpandEnvironmentVariables("%SystemRoot%") + @"\System32\cmd.exe"; //Sets the FileName property of myProcessInfo to %SystemRoot%\System32\cmd.exe where %SystemRoot% is a system variable which is expanded using Environment.ExpandEnvironmentVariables
+            //myProcessInfo.Arguments = "&"; //Sets the arguments to cd..
+            myProcessInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; //Sets the WindowStyle of myProcessInfo which indicates the window state to use when the process is started to Hidden
+            myProcessInfo.Verb = "runas"; //The process should start with elevated permissions
+
+            myProcessInfo.RedirectStandardInput = true;
+            myProcessInfo.RedirectStandardOutput = true;
+            myProcessInfo.UseShellExecute = false;
+
+            var process = Process.Start(myProcessInfo);
+
+            if (process != null)
+            {
+                process.StandardInput.WriteLine("cd C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319");
+
+                foreach (var node in _nodeArray)
+                {
+                    string commandString = "@ASPNET_REGIIS -pdf " + "" + node + "" + " " + "" + _webConfigFolder + "";
+                    process.StandardInput.WriteLine(commandString);
+                }
+
+                //process.StandardInput.WriteLine("yourCommand.exe arg1 arg2");
+
+                process.StandardInput.Close(); // line added to stop process from hanging on ReadToEnd()
+
+                var outputString = process.StandardOutput.ReadToEnd();
+                return outputString;
+            }
+            return string.Empty;
+        }
+    }
+}
